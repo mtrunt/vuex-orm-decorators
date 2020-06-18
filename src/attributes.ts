aa5d775a42d86bbcd7897fa992c9b564337ab2cb
User <mtru92@gmail.com>
2020-06-18 18:44:11 -0500

    Set type as generic function

--- src/attributes.ts
+++ src/attributes.ts
@@ -40,7 +40,7 @@ export function IncrementField() {
  * Adds the property as a generic attribute field
  * @param defaultValue The default value for the field (if undiefine dthe default will be '')
  */
-export function AttrField(defaultValue?: any, mutator?: Mutator<unknown>) {
+export function AttrField(defaultValue?: any, mutator?: Mutator<Function>) {
     return Field(Model.attr(defaultValue, mutator));
 }
 
